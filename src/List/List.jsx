303bb67d47004722fc06f57f3396d43df5cad39a
banushanv <banushanv@gmail.com>
2022-09-04 21:21:28 +0530

    added style in separate file

--- src/List/List.jsx
+++ src/List/List.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import useStyles from './styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -9,7 +9,6 @@ import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -21,42 +20,6 @@ import Select from '@material-ui/core/Select';
 import Rating from '@material-ui/lab/Rating';
 import Box from '@material-ui/core/Box';
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 380,
-    marginTop: 15,
-    marginLeft: 15
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-  button: {
-    display: 'block',
-    marginTop: theme.spacing(2),
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  rating: {
-    paddingRight: '16.9%', // 16:9
-  },
-}));
 
 const RecipeReviewCard=()=> {
   const classes = useStyles();
--- src/List/styles.js
+++ src/List/styles.js
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-
+import { red } from '@material-ui/core/colors';
 export default makeStyles((theme) => ({
     formControl: {
         margin: theme.spacing(1), minWidth: 120, marginBottom: '30px',
@@ -19,4 +19,33 @@ export default makeStyles((theme) => ({
     list: {
         height: '75vh', overflow: 'auto',
     },
+    root: {
+        maxWidth: 380,
+        marginTop: 15,
+        marginLeft: 15
+      },
+      media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+      },
+      expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+          duration: theme.transitions.duration.shortest,
+        }),
+      },
+      expandOpen: {
+        transform: 'rotate(180deg)',
+      },
+      avatar: {
+        backgroundColor: red[500],
+      },
+      button: {
+        display: 'block',
+        marginTop: theme.spacing(2),
+      },
+      rating: {
+        paddingRight: '16.9%', // 16:9
+      }
 }));
