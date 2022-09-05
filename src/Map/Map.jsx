06463fb72134a3bbbf76893cdb5e063adc5a6240
amithap <amithap@embla.asia>
2022-09-05 22:42:37 +0530

    added the basic google map

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import { Rating } from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
-    return (<div>
-        Map
-    </div>)
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const cordinates = { lat: 0, lng: 0 }
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyDq3yV93s2oIZtxxB6fD9Eu3O3MsHt7Ei4' }}
+                defaultCenter={cordinates}
+                center={cordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                option={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
+    );
 }
 
 export default Map;
